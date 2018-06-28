aa3ccf7f696d8140fcb00fc4a317c92bc340f935
Emilio Ponce Alcalde <eponce@meteo.cat>
2018-06-28 17:13:57 +0200

    primer commit, gatsby básico

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: `Pandas Eating Lots`
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    },
+    `gatsby-transformer-remark`
+  ]
+};
---
+++ src/layouts/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "gatsby-link";
+
+
+
+export default ({ children, data }) => (
+  <div>
+    <Link to={`/`}>
+      <h3>
+        {data.site.siteMetadata.title}
+      </h3>
+    </Link>
+    <Link to={`/about/`}>
+      About
+    </Link>
+    {children()}
+  </div>
+);
+
+export const query = graphql`
+  query LayoutQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
