e106768357bef74a47a5fb8501f969f9deaa53b2
Emilio <yosoycore@gmail.com>
2018-07-29 18:37:56 +0200

    404 terminada

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,11 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 
- const NotFoundPage = () => (
-  <div>
-    <h1>No encontrado</h1>
-    <p>Esta ruta no existe, mala suerte.</p>
+const NotFoundPage = () => (
   <div style={{ margin: '3rem auto', maxWidth: 1024 }}>
     <Helmet title="Página no encontrada" />
     <h1>Página no encontrada</h1>
+    <img src="@TODO poner imagen" />
     <p>
       Esta ruta no existe. Puedes volver a la{' '}
       <Link to={`/`}>página de inicio</Link>
@@ -17,3 +15,5 @@ import Link from 'gatsby-link'
     </p>
   </div>
 )
+
+export default NotFoundPage
