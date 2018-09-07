578bacb243a33797747a7fcf4b1bc27c8416841c
Emilio Ponce Alcalde <eponce@meteo.cat>
2018-09-07 15:31:49 +0200

    trabajando en avatar

--- src/pages/sobre-mi.js
+++ src/pages/sobre-mi.js
@@ -1,41 +1,61 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import Img from 'gatsby-image'
 import Layout from '../components/layout'
 
-const SobreMi = () => (
+export default ({ data }) => (
   <Layout>
     <div
       style={{ margin: '3rem auto', maxWidth: 1024, padding: '0 10px 0 10px' }}
     >
       <Helmet title="Blog | Sobre mí" />
       <h1>Sobre mí</h1>
-      <p>
-        Soy Ingeniero Informático y dedico gran parte de mi tiempo a desarrollar
-        aplicaciones web. También gestiono un equipo, e incluso de vez en cuando
-        comienzo un <i>Pet Project</i> y no lo acabo.
-      </p>
-      <p>
-        Siento curiosidad por el aspecto técnico y humano de todo aquello en lo
-        que me sumerjo. Sabrás algo más sobre mí en:
-      </p>
-      <ul>
-        <li>
-          <a href="https://twitter.com/soycore">Twitter</a>
-        </li>
-        <li>
-          <a href="https://github.com/emilioponce">Github</a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/emilio-ponce-a319459/">
-            Linkedin
-          </a>
-        </li>
-        <li>
-          <a href="https://www.youtube.com/user/ValenceBandSongs">Youtube</a>
-        </li>
-      </ul>
+      <div style={{ float: 'left', padding: '0 0 0 10px' }}>
+        <Img
+          resolutions={data.file.childImageSharp.resolutions}
+          style={{ borderRadius: '50%' }}
+          title="Dime usted"
+        />
+      </div>
+      <div>
+        <p>
+          Soy Ingeniero Informático y dedico gran parte de mi tiempo a
+          desarrollar aplicaciones web. También gestiono un equipo, e incluso de
+          vez en cuando comienzo un <i>Pet Project</i> y no lo acabo.
+        </p>
+        <p>
+          Siento curiosidad por el aspecto técnico y humano de todo aquello en
+          lo que me sumerjo. Sabrás algo más sobre mí en:
+        </p>
+        <ul>
+          <li>
+            <a href="https://twitter.com/soycore">Twitter</a>
+          </li>
+          <li>
+            <a href="https://github.com/emilioponce">Github</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/emilio-ponce-a319459/">
+              Linkedin
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/user/ValenceBandSongs">Youtube</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </Layout>
 )
 
-export default SobreMi
+export const query = graphql`
+  query {
+    file(relativePath: { regex: "/avatar/" }) {
+      childImageSharp {
+        resolutions(width: 300, height: 300) {
+          ...GatsbyImageSharpResolutions
+        }
+      }
+    }
+  }
+`
