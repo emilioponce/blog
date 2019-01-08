8f530502c9760caec742368c8e411b2f70376b90
Emilio <yosoycore@gmail.com>
2019-01-08 20:18:16 +0000

    heart added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Icon } from 'antd'
 
 import styles from './Footer.module.css'
 
 export default () => (
   <div className={styles.footer}>
-    Desarrollado con <a href="https://www.gatsbyjs.org">Gatsby</a>
+    Desarrollado con{' '}
+    <Icon type="heart" theme="filled" className={styles.heart} /> y{' '}
+    <a href="https://www.gatsbyjs.org">Gatsby</a>
   </div>
 )
